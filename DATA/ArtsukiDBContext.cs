32f19bee26c7f927fd73db72fc30d581ae270e0f
Ben Yedder Saifeddine <saifeddine.benyedder@esprit.tn>
2017-11-08 01:29:28 +0100

    Initial Project

---
+++ DATA/ArtsukiDBContext.cs
@@ -0,0 +1,25 @@
+﻿using DATA.CustomConventions;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA
+{
+
+    public class ArtsukiDBContext : DbContext
+    {
+        public ArtsukiDBContext() : base("name=Artsuki")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Add(new DateTime2Convention());
+
+        }
+    }
+}
---
+++ DATA/Infrastructure/DatabaseFactory.cs
@@ -0,0 +1,28 @@
+﻿using DATA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA.Infrastructure
+{
+    public class DatabaseFactory : Disposable, IDatabaseFactory
+    {
+        private ArtsukiDBContext dataContext;
+        public ArtsukiDBContext DataContext { get { return dataContext; } }
+
+        public DatabaseFactory()
+        {
+            dataContext = new ArtsukiDBContext();
+        }
+
+        protected override void DisposeCore()
+        {
+            // libérer espace mémoire du context
+            if(DataContext!=null)
+            DataContext.Dispose();
+        }
+    }
+
+}
